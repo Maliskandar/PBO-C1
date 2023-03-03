21d4ea2ac2ed244146285f95d36f58a09a57716e
Muhammad Akmal Iskandar <88116543+Maliskandar@users.noreply.github.com>
2023-03-03 21:49:09 +0700

    Update MOperasiTitik.java

--- Pertemuan 2/OperasiTitik/src/MOperasiTitik.java
+++ Pertemuan 2/OperasiTitik/src/MOperasiTitik.java
@@ -1,16 +1,21 @@
 /**
  * File : MOperasiTitik.java 25/02/18
  * Penulis : Muhammad Akmal Iskandar
- * Deskripsi : Program utama yang memanfaatkan OperasiTitik
+ * Deskripsi : Kelas yang berisi main dari OperasiTitik
  */
 public class MOperasiTitik {
     public static void main(String[] args) {
         Titik t = new Titik(4.0, 4.0);
         OperasiTitik o = new OperasiTitik();
+
         System.out.println("Titik : (" + t.getAbsis() + "," + t.getOrdinat() + ")");
-        o.refleksiSumbuX(t); // hasil ?
+
+        o.refleksiX(t);
+        System.out.println("\n Refleksi Terhadap Sumbu X");
         System.out.println("Titik : (" + t.getAbsis() + "," + t.getOrdinat() + ")");
-        o.refleksiSumbuY(t); // hasil ?
+
+        o.refleksiY(t);
+        System.out.println("\n Refleksi Terhadap Sumbu Y");
         System.out.println("Titik : (" + t.getAbsis() + "," + t.getOrdinat() + ")");
 
     }
