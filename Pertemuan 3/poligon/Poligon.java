24aa3966411383e97aea199b6d359cd90fe9f31b
Muhammad Akmal Iskandar <88116543+Maliskandar@users.noreply.github.com>
2023-03-27 20:41:26 +0700

    Update Poligon.java

--- Pertemuan 3/poligon/Poligon.java
+++ Pertemuan 3/poligon/Poligon.java
@@ -1,3 +1,8 @@
+/*
+* File : MPoligon.java 08/03/2023
+* Penulis : Muhammad Akmal Iskandar
+* Deskripsi : representasi dasar dari objek poligon (segi banyak)
+* */
 package org.poligon;
 
 public class Poligon {
