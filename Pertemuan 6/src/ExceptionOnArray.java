f17951fbb8d0c8c12ee1e90aa849e8baca7ffa45
Muhammad Akmal Iskandar <88116543+Maliskandar@users.noreply.github.com>
2023-05-30 05:48:54 +0700

    Update ExceptionOnArray.java

--- Pertemuan 6/src/ExceptionOnArray.java
+++ Pertemuan 6/src/ExceptionOnArray.java
@@ -1,3 +1,8 @@
+/**
+ * File : ExceptionOnArray.java 30/05/2023
+ * Penulis : Muhammad Akmal Iskandar
+ * Deskripsi : Program penggunaan eksepsi menggunakan class library java
+ */
 public class ExceptionOnArray {
     public static void main(String[] args){
         Integer[] arrayInteger = new Integer[4];
