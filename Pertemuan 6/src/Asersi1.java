5e05df5aedadb19374cc2739e19cd697ffb30a2d
Muhammad Akmal Iskandar <88116543+Maliskandar@users.noreply.github.com>
2023-05-30 05:49:33 +0700

    Update Asersi1.java

--- Pertemuan 6/src/Asersi1.java
+++ Pertemuan 6/src/Asersi1.java
@@ -1,3 +1,8 @@
+/**
+ * File : Asersi1.java 30/05/2023
+ * Penulis : Muhammad Akmal Iskandar
+ * Deskripsi : Program untuk menunjukkan asersi
+ */
 class Asersi1{
     public static void  main(String[] args) {
         int x = 0;
